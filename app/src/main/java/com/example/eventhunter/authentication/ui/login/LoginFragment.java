3708e90a3a11ddf609cdf48fb77c8add87cbd541
Andrei Timar <atimar999@gmail.com>
2021-04-07 11:41:36 +0300

    Added Login Controller

--- app/src/main/java/com/example/eventhunter/authentication/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/eventhunter/authentication/ui/login/LoginFragment.java
@@ -7,19 +7,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.eventhunter.authentication.AuthenticationService;
 import com.example.eventhunter.databinding.LoginFragmentBinding;
+import com.example.eventhunter.di.Injectable;
+import com.example.eventhunter.di.ServiceLocator;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import static com.example.eventhunter.MainActivity.AUTH_ACTIVITY_REQUEST_EXTRA;
 
 public class LoginFragment extends Fragment {
 
-    private LoginViewModel mViewModel;
+    @Injectable()
+    private AuthenticationService authenticationService;
+
+    public LoginFragment() {
+        ServiceLocator.getInstance().inject(this);
+    }
+
     private LoginFragmentBinding binding;
 
     public static LoginFragment newInstance() {
@@ -30,24 +39,34 @@ public class LoginFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         binding = LoginFragmentBinding.inflate(inflater, container, false);
-        mViewModel = new ViewModelProvider(requireActivity()).get(LoginViewModel.class);
+
         binding.loginButton.setOnClickListener(view -> {
-            Intent intent = new Intent();
-            intent.putExtra(AUTH_ACTIVITY_REQUEST_EXTRA, "login");
-            getActivity().setResult(Activity.RESULT_OK, intent);
-            getActivity().finish();
+            String email = binding.editTextEmailAddress.getText().toString();
+            String password = binding.editTextPassword.getText().toString();
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Snackbar.make(view, "Some Fields Are Empty", Snackbar.LENGTH_SHORT)
+                        .show();
+                return;
+            }
+
+            authenticationService.login(email, password, errorMessage -> {
+                if (errorMessage == null) {
+                    Intent intent = new Intent();
+                    intent.putExtra(AUTH_ACTIVITY_REQUEST_EXTRA, "login");
+                    getActivity().setResult(Activity.RESULT_OK, intent);
+                    getActivity().finish();
+                } else {
+                    Snackbar.make(view, errorMessage, Snackbar.LENGTH_SHORT)
+                            .show();
+                }
+            });
         });
         binding.goToRegister.setOnClickListener(view -> {
             Navigation.findNavController(view).navigate(LoginFragmentDirections.navigateToRegister());
         });
-        mViewModel.getEmailAddress().observe(getViewLifecycleOwner(), email -> {
-            binding.editTextEmailAddress.setText(email);
-        });
-        mViewModel.getPassword().observe(getViewLifecycleOwner(), password -> {
-            binding.editTextPassword.setText(password);
-        });
-        View view = binding.getRoot();
-        return view;
+
+        return binding.getRoot();
     }
 
     @Override
