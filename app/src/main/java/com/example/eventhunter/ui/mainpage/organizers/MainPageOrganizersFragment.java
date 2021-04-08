b432346ccb27e7515de1d4261980ef89f5fb2ae3
Tania Topciov <taniatopciov@yahoo.com>
2021-04-08 15:17:16 +0300

    Modified MainPageOrganizersFragment and MainPageOrganizersViewModel

--- app/src/main/java/com/example/eventhunter/ui/mainpage/organizers/MainPageOrganizersFragment.java
+++ app/src/main/java/com/example/eventhunter/ui/mainpage/organizers/MainPageOrganizersFragment.java
@@ -9,22 +9,53 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.eventhunter.R;
+import com.example.eventhunter.databinding.FragmentHomeCollaboratorsBinding;
+import com.example.eventhunter.databinding.FragmentHomeOrganizersBinding;
+import com.example.eventhunter.ui.mainpage.collaborators.MainPageCollaboratorsFragment;
+import com.example.eventhunter.ui.mainpage.collaborators.MainPageCollaboratorsViewModel;
+import com.example.eventhunter.ui.mainpage.collaborators.collaboratorCard.CollaboratorCard;
+import com.example.eventhunter.ui.mainpage.collaborators.collaboratorCard.CollaboratorCardAdapter;
+import com.example.eventhunter.ui.mainpage.organizers.organizerCard.OrganizerCard;
+import com.example.eventhunter.ui.mainpage.organizers.organizerCard.OrganizerCardAdapter;
 
 public class MainPageOrganizersFragment extends Fragment {
 
     private MainPageOrganizersViewModel mainPageOrganizersViewModel;
+    private FragmentHomeOrganizersBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        mainPageOrganizersViewModel =
-                new ViewModelProvider(this).get(MainPageOrganizersViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        mainPageOrganizersViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
-        return root;
+    public static MainPageOrganizersFragment newInstance() {
+        return new MainPageOrganizersFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentHomeOrganizersBinding.inflate(inflater, container, false);
+        mainPageOrganizersViewModel = new ViewModelProvider(requireActivity()).get(MainPageOrganizersViewModel.class);
+
+        RecyclerView organizersRecycleView = binding.homeOrganizersRecyclerView;
+        OrganizerCard[] organizers = {new OrganizerCard("Name1", "name1@example.com","Music Events"), new OrganizerCard("Name2", "name2@example.com", "Literature Events"), new OrganizerCard("Name3", "name3@example.com", "Sportive Events")};
+
+        organizersRecycleView.setLayoutManager(new LinearLayoutManager(requireActivity()));
+        organizersRecycleView.setAdapter(new OrganizerCardAdapter(organizers));
+
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
