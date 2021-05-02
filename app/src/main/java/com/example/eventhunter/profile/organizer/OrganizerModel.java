176f7120429d752913f47a28f878bb95a5838457
Tania Topciov <taniatopciov@yahoo.com>
2021-05-02 23:47:37 +0300

    Added RegularUserModel, ReservationModel, OrganizerModel

---
+++ app/src/main/java/com/example/eventhunter/profile/organizer/OrganizerModel.java
@@ -0,0 +1,16 @@
+package com.example.eventhunter.profile.organizer;
+
+import android.graphics.Bitmap;
+
+public class OrganizerModel {
+
+    public String id;
+    public String name;
+    public String userType;
+    public String email;
+    public String phoneNumber;
+    public String address;
+    public Bitmap profilePhoto;
+    public int organizedEvents;
+    public String eventType;
+}
