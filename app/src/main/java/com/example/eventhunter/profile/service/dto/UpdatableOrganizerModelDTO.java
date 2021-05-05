32a8fafb8b61336f9a22a3118fa3c3f65ad12f62
Tania Topciov <taniatopciov@yahoo.com>
2021-05-05 23:14:55 +0300

    Added Updatable Model for Collaborator and Organizer

---
+++ app/src/main/java/com/example/eventhunter/profile/service/dto/UpdatableOrganizerModelDTO.java
@@ -0,0 +1,9 @@
+package com.example.eventhunter.profile.service.dto;
+
+public class UpdatableOrganizerModelDTO {
+
+    public String address;
+    public String phoneNumber;
+    public String eventType;
+    public int organizedEvents;
+}
